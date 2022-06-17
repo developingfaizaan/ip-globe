1cad15bb2c68607ada1b7ff8a1d9a0f131b49df0
Faizan <developingfaizaan@gmail.com>
2022-06-17 18:13:23 +0530

    added mapbox map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,8 +1,64 @@
+import { useEffect, useState } from "react";
+import ReactMapGL, { Marker, FlyToInterpolator } from "react-map-gl";
+// Context
+import { useIpDetail } from "../../contexts/IpContext";
+// Component
+import CustomMarker from "./Marker";
+
+// Mapbox Access Token - API KEY
+const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
 const Map = () => {
+  const { currentIpDetail } = useIpDetail();
+  const [lng, setLng] = useState(72.8777); // longitude
+  const [lat, setLat] = useState(19.076); // latitude
+  const [viewport, setViewport] = useState({
+    // camera angle
+    longitude: lng,
+    latitude: lat,
+    zoom: 9,
+    // pitch: 60, bearing: -60, // For map angles
+  });
+
+  useEffect(() => {
+    // Getting lon, lat from api response
+    const { lon: longitude, lat: latitude } = currentIpDetail;
+
+    if (!longitude && !latitude) return;
+
+    // Updating the state for the marker on the map by longitude and lotitude
+    setLng(longitude);
+    setLat(latitude);
+
+    // To set the camera angle of the map
+    setViewport({
+      ...viewport,
+      longitude: longitude, // API Response longitude
+      latitude: latitude, // API Response latitude
+      zoom: 10,
+      transitionDuration: 5000,
+      transitionInterpolator: new FlyToInterpolator(),
+    });
+  }, [currentIpDetail]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // custom variable for responsive Map
+  const mapHeight = window.innerWidth > 1080 ? "100vh" : "80vh";
+
   return (
-    <div>
-      <h1>Map</h1>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      width="100%"
+      height={mapHeight} // custom variable for responsive map
+      mapStyle="mapbox://styles/mapbox/dark-v8"
+      onViewportChange={(viewport) => setViewport(viewport)}
+      onViewStateChange={(viewport) => setViewport(viewport)}
+      attributionControl={false}
+      mapboxApiAccessToken={TOKEN}
+    >
+      <Marker latitude={lat} longitude={lng} offsetLeft={-20} offsetTop={-10}>
+        <CustomMarker />
+      </Marker>
+    </ReactMapGL>
   );
 };
 
